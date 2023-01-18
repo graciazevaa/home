24720492af11f2cedcbfcd0cc83bcd864bfe439f
graciazevaa <122952570+graciazevaa@users.noreply.github.com>
2023-01-18 11:01:41 +0700

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/hashirshoaeb/hashirshoaeb.github.io.git";
+const repoURL = "https://github.com/graciazevaa/graciazevaa.github.io.git";
 
 ghpages.publish(
   pathname,
